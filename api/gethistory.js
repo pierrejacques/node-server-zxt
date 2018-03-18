1fe2c1ac344bf09a64c8004f95647362f6051318
金辰浩 <jinchenhao@meituan.com>
2018-03-18 19:56:23 +0800

    api dist gethistory

---
+++ api/gethistory.js
@@ -0,0 +1,48 @@
+const moment = require('moment');
+const db = require('../database')();
+
+/* GET */
+
+// FIXME: 由于不合理的数据维护结构导致获取历史树的过程极为啰嗦且低效
+
+module.exports = async function (req, res, next) {
+    const _id = req.cookies.user_id;
+    res.status = 200;
+    if (!_id) {
+        res.send({
+            code: 400,
+            success: false,
+            msg: '没有权限',
+        });
+        return;
+    }
+    const user = await db.findUser({ _id });
+    const treeInfos = Array.apply(null, { length: user.trees.length });
+    const task = new Promise((resolve, reject) => {
+        num = user.trees.length;
+        count = 0;
+        user.trees.forEach((_id, idx) => {
+            db.findTree(_id).then(tree => {
+                count ++;
+                treeInfos[idx] = moment(tree.createOn).format('YYYY-MM-DD hh:mm:ss');
+                if (count === num) {
+                    resolve();
+                }
+            }, reject);
+        });
+    });
+    task.then(() => {
+        res.send({
+            code: 200,
+            msg: '',
+            success: true,
+            data: treeInfos,
+        });
+    }, () => {
+        res.send({
+            code: 500,
+            msg: '读取家谱错误',
+            success: false,
+        });
+    });
+}
--- api/index.js
+++ api/index.js
@@ -6,11 +6,13 @@ const saveTree = require('./saveTree');
 const userinfo = require('./userinfo');
 const latestTree = require('./latestTree');
 const logout = require('./logout');
+const gethistory = require('./gethistory');
 
 router.get('/userinfo', userinfo);
 router.get('/checkusername', checkusername);
 router.get('/latestTree', latestTree);
 router.get('/logout', logout);
+router.get('/gethistory', gethistory);
 router.post('/regist', regist);
 router.post('/login', login);
 router.post('/saveTree', saveTree);
