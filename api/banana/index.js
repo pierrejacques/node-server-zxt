a49ebb7b150d008746f9160ed0eb0e164414c91b
金辰浩 <jinchenhao@meituan.com>
2018-05-30 23:42:41 +0800

    delete Record

--- api/banana/index.js
+++ api/banana/index.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const getRecord = require('./getRecord');
 const createRecord = require('./createRecord');
+const deleteRecord = require('./deleteRecord');
 const getComponent = require('./getComponent');
 const createComponent = require('./createComponent');
 const deleteComponent = require('./deleteComponent');
@@ -8,6 +9,7 @@ const getStatisticTypes = require('./getStatisticTypes');
 
 router.get('/record/get', getRecord);
 router.post('/record/create', createRecord);
+router.get('/record/delete', deleteRecord);
 router.get('/component/get', getComponent);
 router.get('/component/create', createComponent);
 router.get('/component/delete', deleteComponent);
@@ -15,8 +17,6 @@ router.get('/statistic/type/get', getStatisticTypes);
 
 module.exports = router;
 
-// TODO: record编辑
 // TODO: record按时间范围查询
-// TODO: record删除
 // TODO: record导出
 // TODO: record导入
